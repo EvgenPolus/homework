ab6912f47c69c91dbe857e6c8b57a1584afc98a3
EvgenPolus <teh.polus@gmail.com>
2024-11-14 02:10:43 +0300

    12.11.2024  module_5_3.py

--- Project_Class/module_5_3.py
+++ Project_Class/module_5_3.py
@@ -17,26 +17,53 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-    def __eq__(self, other):
-        return self.number_of_floors == other
+    @classmethod
+    def __check_dat(cls, other):
+        if isinstance(other, House):
+            other = other.number_of_floors
+        elif isinstance(other, int):
+            other = other
+        return other
 
-    def __add__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors += value
+    def __eq__(self, other):
+        oth = self.__check_dat(other)
+        return self.number_of_floors == oth
+
+    def __add__(self, other):
+        oth = self.__check_dat(other)
+        self.number_of_floors += oth
         return self
 
     def __iadd__(self, other):
-        return self.__add__(other)
+        oth = self.__check_dat(other)
+        self.number_of_floors += oth
+        return self
 
     def __radd__(self, other):
-        if isinstance(other, int):
-            other += self.number_of_floors
-            self.number_of_floors = other
+        oth = self.__check_dat(other)
+        oth += self.number_of_floors
+        self.number_of_floors = oth
         return self
 
     def __lt__(self, other):
-        if isinstance(other, int):
-            return self.number_of_floors < other
+        oth = self.__check_dat(other)
+        return self.number_of_floors < oth
+
+    def __gt__(self, other):
+        oth = self.__check_dat(other)
+        return self.number_of_floors > oth
+
+    def __ge__(self, other):
+        oth = self.__check_dat(other)
+        return self.number_of_floors >= oth
+
+    def __le__(self, other):
+        oth = self.__check_dat(other)
+        return self.number_of_floors <= oth
+
+    def __ne__(self, other):
+        oth = self.__check_dat(other)
+        return self.number_of_floors != oth
 
 
 
@@ -47,7 +74,7 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2)
+print(h1 == h2) # __eq__
 
 h1 = h1 + 10 # __add__
 print(h1)
@@ -59,8 +86,8 @@ print(h1)
 h2 = 10 + h2 # __radd__
 print(h2)
 
-#print(h1 > h2) # __gt__
-# print(h1 >= h2) # __ge__
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
 print(h1 < h2) # __lt__
-# print(h1 <= h2) # __le__
-# print(h1 != h2) # __ne__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
